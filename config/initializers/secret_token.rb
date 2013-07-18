ac1680fe11554ec2f0369726f0491077f23602e9
Robin Moore <horsedonkey@gmail.com>
2013-07-17 19:36:03 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LawyerTools::Application.config.secret_token = '186c519f5cd7272fc0275a59a462a0946d37a6dedc66e3f962cea1538879f9c6e734a4000091b54f4485e83592f10aeb00e530336758551bbef2c1f454d3be4a'
